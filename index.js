17fcb4cca99f10ac6cbc25b90dd16cde7e1b51c7
tpham20908 <tpham20908@gmail.com>
2018-09-13 22:33:12 -0400

    created User.js with mongoose

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+require("./models/User");
+
+mongoose.connect(keys.mongoURI, () => {console.log("Connected to MongoDB")});
 
 const app = express();
 require("./routes/authRoutes")(app);
