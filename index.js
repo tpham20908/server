88a2c9c9c66a485b8884fbfc6714ca5291108542
tpham20908 <tpham20908@gmail.com>
2018-09-13 21:01:01 -0400

    added nodemon

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const app = express();
+require("./services/passport");
 
-app.get("/", (req, res) => {
-  res.send({ Bye: "Buddy" });
-});
+const app = express();
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log("App is listening at port: " + PORT);
+});
