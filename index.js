114fe71abee2a1fb0af8c7a26899beedc6d338ce
tpham20908 <tpham20908@gmail.com>
2018-09-14 23:12:50 -0400

    created cookie with cookie-session

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
-require("./services/passport");
 require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI, () => {console.log("Connected to MongoDB")});
 
 const app = express();
+
+app.use(new cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
