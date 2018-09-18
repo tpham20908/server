bcc44c9c9cfb08b5d9100e84a723e0ccb48739f3
tpham20908 <tpham20908@gmail.com>
2018-09-17 22:26:06 -0400

    styling Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+
+const Landing = () => <h2>Landing</h2>
+const SurveyNew = () => <h2>Survey New</h2>
+const DashBoard = () => <h2>DashBoard</h2>
 
 const App = () => {
   return (
     <div>
-      Halleluiha!!!
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={Landing} />
+          <Route path="/surveys" exact component={DashBoard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
